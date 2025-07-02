3110f260fd5f09ca8870119b96641f659e2bab2d
kimywann <kimywan10@gmail.com>
2025-07-02 22:29:50 +0900

    feat: 라우팅 시스템 초기 설정 및 페이지 라우트 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { RouterProvider } from "react-router";
+import router from "./route";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-200">Hello World</h1>
+      <RouterProvider router={router} />
     </>
   );
 }
--- src/route.tsx
+++ src/route.tsx
@@ -1,9 +1,33 @@
 import { createBrowserRouter } from "react-router";
+import HomePage from "./features/home/home-page";
+import Layout from "./layout";
+import SignInPage from "./common/components/Sign/Sign-in-page";
+import SignUpPage from "./common/components/Sign/Sign-up-page";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: (
+      <Layout>
+        <HomePage />
+      </Layout>
+    ),
+  },
+  {
+    path: "/signin",
+    element: (
+      <Layout>
+        <SignInPage />
+      </Layout>
+    ),
+  },
+  {
+    path: "/signup",
+    element: (
+      <Layout>
+        <SignUpPage />
+      </Layout>
+    ),
   },
 ]);
 
