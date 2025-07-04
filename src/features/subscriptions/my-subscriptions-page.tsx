efef0cb5c899d5f275424100d7706b13a79aac98
kimywann <kimywan10@gmail.com>
2025-07-04 19:44:38 +0900

    feat: MySubscriptionsPage에 ServiceBox 연결

---
+++ src/features/subscriptions/my-subscriptions-page.tsx
@@ -0,0 +1,11 @@
+import ServiceBox from "../service/components/service-box";
+
+function MySubscriptionsPage() {
+  return (
+    <>
+      <div className="mt-12 rounded-lg border border-slate-300 p-48"></div>
+      <ServiceBox />
+    </>
+  );
+}
+export default MySubscriptionsPage;
