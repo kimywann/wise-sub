ddc52c6a7eb0fceb61c81460adc43b5e1a9379e3
kimywann <kimywan10@gmail.com>
2025-07-04 19:44:27 +0900

    feat: ServiceBox 컴포넌트로 서비스 목록 카테고리별 출력

---
+++ src/features/service/components/service-box.tsx
@@ -0,0 +1,25 @@
+import { SERVICES_LIST } from "../constants/service-list";
+import ServiceCard from "./service-card";
+
+export default function ServiceBox() {
+  return (
+    <>
+      {Object.entries(SERVICES_LIST).map(([category, services]) => (
+        <div key={category}>
+          <p className="mb-2 text-lg font-semibold">{category}</p>
+
+          <div className="mb-8 flex gap-6">
+            {services.map((service) => (
+              <div
+                key={service.name}
+                className="flex h-30 w-30 cursor-pointer flex-col rounded-2xl border border-slate-300 hover:border-indigo-500"
+              >
+                <ServiceCard service={service} />
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
