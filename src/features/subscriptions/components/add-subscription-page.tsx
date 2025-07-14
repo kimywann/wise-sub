df2117323331206a50a0721ed7ef13e7cc88bad7
kimywann <kimywan10@gmail.com>
2025-07-14 10:54:41 +0900

    feat: 뒤로가기 버튼 추가

--- src/features/subscriptions/components/add-subscription-page.tsx
+++ src/features/subscriptions/components/add-subscription-page.tsx
@@ -1,17 +1,31 @@
 import ServiceBox from "./service/components/service-box";
+import { useNavigate } from "react-router-dom";
+import leftIcon from "@/assets/icon/left.svg";
 
 function AddSubscriptionPage() {
+  const navigate = useNavigate();
+
   return (
     <>
-      <header className="mt-20 mb-8 flex flex-col items-center justify-center">
-        <div className="mb-2 text-3xl font-bold text-indigo-600">
-          구독 서비스 추가
-        </div>
-        <p className="text-md mb-5 text-slate-500">클릭 하면 추가 됩니다.</p>
-        <div>
-          <ServiceBox />
+      <header className="relative mt-20 mb-8 flex w-full items-center justify-center">
+        <img
+          src={leftIcon}
+          alt="뒤로가기"
+          className="absolute top-1/2 left-15 h-10 w-10 -translate-y-1/2 cursor-pointer"
+          onClick={() => navigate("/subscription")}
+        />
+
+        <div className="text-center">
+          <h1 className="mb-2 text-3xl font-bold text-indigo-600">
+            구독 서비스 추가
+          </h1>
+          <p className="text-md text-slate-500">클릭 하면 추가 됩니다.</p>
         </div>
       </header>
+
+      <main className="flex items-center justify-center">
+        <ServiceBox />
+      </main>
     </>
   );
 }
