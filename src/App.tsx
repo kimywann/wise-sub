15279b6b24aa180c7aae55ea6ce2a9d5990ee892
kimywann <kimywan10@gmail.com>
2025-07-25 22:02:59 +0900

    feat: @supabase/auth-helpers-react 설치 및 useUser 훅 적용 - 로그인 유저 ID 기반으로 구독 데이터 삽입 구현 준비 - 구독 추가 시 useUser() 통해 user_id 자동 입력

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from "react-router";
 import router from "./route";
 
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { supabase } from "./lib/supabaseClient";
+
 function App() {
   return (
-    <>
+    <SessionContextProvider supabaseClient={supabase}>
       <RouterProvider router={router} />
-    </>
+    </SessionContextProvider>
   );
 }
 
