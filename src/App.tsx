8cd8a5869dee44a4c84adb8a1296c2b843f6034e
kimywann <kimywan10@gmail.com>
2025-07-28 16:54:37 +0900

    chore: sonner toast 라이브러리 추가 및 전역 설정

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import router from "./route";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { supabase } from "./lib/supabaseClient";
 
+import { Toaster } from "sonner";
+
 function App() {
   return (
     <SessionContextProvider supabaseClient={supabase}>
+      <Toaster richColors position="top-right" />
       <RouterProvider router={router} />
     </SessionContextProvider>
   );
