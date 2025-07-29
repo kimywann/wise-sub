7b6bd295fec6345e5488ee020cb81c7a549d7cc2
kimywann <kimywan10@gmail.com>
2025-07-29 21:17:15 +0900

    feat: '직접입력' 서비스 카드에 plus 아이콘 이미지 표시하도록 분기 처리

--- src/features/subscription/components/service/components/service-card.tsx
+++ src/features/subscription/components/service/components/service-card.tsx
@@ -1,4 +1,5 @@
 import { type ServiceItem } from "../constants/service-list";
+import plus from "@/assets/icon/plus.svg";
 
 interface ServiceCardProps {
   service: ServiceItem;
@@ -7,11 +8,15 @@ interface ServiceCardProps {
 export default function ServiceCard({ service }: ServiceCardProps) {
   return (
     <div className="flex flex-col items-center text-center">
-      <img
-        src={service.image}
-        alt={service.name}
-        className="mt-4 h-[4.5rem] w-[4.5rem]"
-      />
+      {service.name === "직접입력" ? (
+        <img src={plus} className="mt-4 h-[4.5rem] w-[4.5rem]" />
+      ) : (
+        <img
+          src={service.image}
+          alt={service.name}
+          className="mt-4 h-[4.5rem] w-[4.5rem]"
+        />
+      )}
       <span className="font-medium">{service.name}</span>
     </div>
   );
